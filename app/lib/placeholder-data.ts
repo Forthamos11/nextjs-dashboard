606d3a9043652e2b400f17ab1721dd44f173597a
Jannis Heydemann <heydemannjannis111@gmail.com>
2025-02-15 12:53:57 +0100

    First Push to the hub

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -3,8 +3,8 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
+    name: 'Jannis',
+    email: 'Jannis@nextmail.com',
     password: '123456',
   },
 ];
